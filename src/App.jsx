eda3b2d4f0e823f79146f837f749513a316d2c98
Samuele-Finotello <samuelefinotello.147@gmail.com>
2025-09-08 17:32:30 +0200

    [add] feat create input and radios

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,46 @@ import axios from "axios"
 
 function App() {
 
-  return (
-    <div>
+  const [posts, setPosts] = useState([])
 
+  const fetchPosts = () => {
+    axios.get('https://67c5b4f3351c081993fb1ab6.mockapi.io/api/posts').then((resp) => {
+      setPosts(resp.data)
+      console.log(resp.data)
+    })
+  }
+
+  return (
+    <div className="container mt-5">
+      <div className="row">
+        <div className="col-12">
+          <form>
+            <div className="fs-5">
+              <input type="text" name="author" className="form-control fs-4" placeholder="Inserisci autore" />
+              <input type="text" name="title" className="form-control fs-4" placeholder="Inserisci titolo" />
+              <input type="text" name="body" className="form-control fs-4" placeholder="Inserisci testo del post" />
+            </div>
+            <div>
+              <label className="fs-4">Vuoi pubblicarlo?</label>
+            </div>
+            <div className="form-check fs-4">
+              <input type="radio" name="radioDefault" />
+              <label className="ms-2">
+                Si
+              </label>
+            </div>
+            <div className="form-check fs-4">
+              <input type="radio" name="radioDefault" />
+              <label className="ms-2">
+                No
+              </label>
+            </div>
+          </form>
+        </div>
+        <div className="col-12">
+          <button className="btn btn-primary" onClick={() => { alert('Post caricato') }}>Carica post</button>
+        </div>
+      </div>
     </div>
   )
 }
